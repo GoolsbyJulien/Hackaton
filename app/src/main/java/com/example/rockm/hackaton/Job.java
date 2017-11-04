83218ba01fb6959ae5aed1252e09c0ba97425fd0
Julien Goolsby <juliengoolsby@gmail.com>
2017-11-04 13:11:09 -0500

    Init

---
+++ app/src/main/java/com/example/rockm/hackaton/Job.java
@@ -0,0 +1,12 @@
+package com.example.rockm.hackaton;
+
+/**
+ * Created by rockm on 11/4/2017.
+ */
+
+public class Job {
+    private String name;
+    private String money;
+    private String location;
+    private String email,phone;
+}
