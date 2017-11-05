bea2ce30745c5529e3f4c8ba01015001906996d9
Julien Goolsby <juliengoolsby@gmail.com>
2017-11-04 21:07:48 -0500

    Initsdf

--- app/src/main/java/com/example/rockm/hackaton/Career.java
+++ app/src/main/java/com/example/rockm/hackaton/Career.java
@@ -1,5 +1,6 @@
 package com.example.rockm.hackaton;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -12,7 +13,7 @@ import android.widget.Toast;
 import java.util.ArrayList;
 import java.util.List;
 
-public class Carrer extends AppCompatActivity {
+public class Career extends AppCompatActivity {
     private double total_hours;
 
     private ArrayList<Job> comJobs = new ArrayList<>();
@@ -31,23 +32,34 @@ public class Carrer extends AppCompatActivity {
 
         list = (ListView) findViewById(R.id.listVeiw);
 
-        AdapterViewCustom adapter = new AdapterViewCustom(this, comJobs);
-        list.setAdapter(adapter);
-
         list.setOnItemClickListener(new android.widget.AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(android.widget.AdapterView<?> parent,
                                     View view, int position, long id) {
-                Toast.makeText(getApplicationContext(),
-                        comJobs.get(position).getName(), Toast.LENGTH_LONG).show();
+                System.out.print("working");
+                open(comJobs.get(position));
             }
         });
+        AdapterViewCustom adapter = new AdapterViewCustom(this, comJobs);
+        list.setAdapter(adapter);
+
+
 
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        setSupportActionBar(toolbar);}
+
+public void open(Job j) {
+        Intent i = new Intent(this,jobScreen.class);
 
 
-    }public void createJobs() {
+        i.putExtra("Name", j.getName());
+        i.putExtra("Describe",j.getDescription());
+        startActivity(i);
+
+    }
+
+
+    public void createJobs() {
 
         comJobs.add(new Job("McDonalds","29$ per hour", "Kansas city", "test@gmail.com","111-111-111" ,"Manager"));
         comJobs.add(new Job("Starbucks ","29$ per hour", "Kansas city", "test@gmail.com","111-111-111" ,"Barista"));
